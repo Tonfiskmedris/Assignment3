f4118c72f1f5ce16194ff5123024b01f3b611c58
Liwecorp <40519864+Liwecorp@users.noreply.github.com>
2021-04-16 22:11:48 +0200

    Assignment 3 Problem A - klar Problem B - klar Problem C - huvuduppgift klar (lite småsaker kvar bara) Problem D - skapat projektet

--- src/problemd/Main.java
+++ src/problemd/Main.java
@@ -1,8 +1,53 @@
 package problemd;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    Scanner input = new Scanner(System.in);
+    private String[] queueSize = new String[10];
+
     public static void main(String[] args) {
 
+        Main myApp = new Main();
+        int choice;
+
+        //kör programmet tills att användaren valt att avsluta programmet.
+        do {
+            myApp.displayMenu();
+            System.out.println("Input action: ");
+            choice = Integer.parseInt(myApp.input.nextLine());
+
+            switch (choice) {
+                case 1 -> myApp.createPhoneCall();
+                case 2 -> myApp.answerPhoneCall();
+            }
+        } while (choice != 3);
+    }
+
+    private void displayMenu() {
+        System.out.println();
+        System.out.println("|  ------- Menu -------   |");
+        System.out.println("| 1. Add phone call       |");
+        System.out.println("| 2. Answer phone call    |");
+        System.out.println("| 3. Exit                 |");
+        System.out.println("| ----------------------- |");
+        System.out.println();
+    }
+
+    private void createPhoneCall() {
+        //få metoden att lägga till ett element i arrayen vid varje exekvering.
+        PhoneCall createPhoneCall = new PhoneCall();
+        System.out.println("Enter your name: ");
+        createPhoneCall.setName(input.nextLine());
+        System.out.println("Enter phone number: ");
+        createPhoneCall.setNumber(Integer.parseInt(input.nextLine()));
+
+        //for(int i = 0; i < queuesize.lenght; i++) vi vill få programmet så att den endast kan addera 10 element till listan dvs max kö = 10 personer.
+    }
+
+    private void answerPhoneCall() {
+        // ta bort från array listan och printa vem vi svarar: namn (telnr).
     }
 }
 
--- src/problemd/PhoneCall.java
+++ src/problemd/PhoneCall.java
@@ -1,4 +1,23 @@
 package problemd;
 
 public class PhoneCall {
+    String name;
+    int number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
+
